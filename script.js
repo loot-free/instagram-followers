b6fe238520d056345fa7ed5562d86c93a1d43095
loot-free <ffousiya720@gmail.com>
2026-09-02 18:52:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function sub()
 {
-    if(txtpass.length <= 8)
+    var value = document.frm1.testValue.value;
+
+    if(value.length <= 8)
     {
-        alert("Password must be more than 8 digits");
+        alert("Value must be more than 8 characters");
     }
 }
